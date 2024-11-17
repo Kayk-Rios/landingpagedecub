e4ec05b5ee71d00d0c7a694f58b5cf5c89214e69
Kayk-Rios <manjac688@gmail.com>
2024-11-17 01:31:33 -0300

    feat:homeLandigPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import Image from "next/image";
+import Footer from "@/components/footer/footer";
+import ImageEnfermeira2 from "@/components/imagemEmfermeira/ImageEnfermeira2";
 
 export default function Home() {
   return (
-   <>
-   
-   </>
+    <>
+      <div className="flex flex-col ">
+        <section className="flex justify-between items-center mb-14">
+
+          <div className="flex flex-col pl-[30px] ">
+             <a href="/layout" className="pb-9 text-white"><h1>Sobre</h1></a>
+            <iframe
+
+              width="580"
+              height="415"
+              src="https://www.youtube.com/embed/6wW-Z3KZnt4?si=eWTauZpI_Xxg1C7J"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+
+            <button className="flex justify-center items-center mt-14  corButao rounded-full">
+              Contatar
+            </button>
+          </div>
+          <div>
+            <ImageEnfermeira2 />
+          </div>
+        </section>
+        <div className="mr-16"><Footer/></div>
+      </div>
+    </>
   );
 }
