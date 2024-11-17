2070379f4ac12fa4cfc9eb44f1e26b8a7ed2571c
Kayk-Rios <manjac688@gmail.com>
2024-11-17 01:56:07 -0300

    feat:contactWhatsap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ export default function Home() {
     <>
       <div className="flex flex-col ">
         <section className="flex justify-between items-center mb-14">
-
           <div className="flex flex-col pl-[30px]  ">
-             <a href="/layout" className="pb-9 text-white"><h1>Sobre</h1></a>
+            <a href="/layout" className="pb-9 text-white">
+              <h1>Sobre</h1>
+            </a>
             <iframe
-                className=" flex sm:-w[300px] md:w-[500px] lg:w-[650px] "
+              className=" flex sm:-w[300px] md:w-[500px] lg:w-[650px] "
               height="415"
               src="https://www.youtube.com/embed/6wW-Z3KZnt4?si=eWTauZpI_Xxg1C7J"
               title="YouTube video player"
@@ -21,14 +22,22 @@ export default function Home() {
             ></iframe>
 
             <button className="flex justify-center items-center mt-14  corButao rounded-full">
-              Contatar
+              <a
+                href="https://wa.me/981699096"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Contatar
+              </a>
             </button>
           </div>
           <div>
             <ImageEnfermeira2 />
           </div>
         </section>
-        <div className="mr-16"><Footer/></div>
+        <div className="mr-16">
+          <Footer />
+        </div>
       </div>
     </>
   );
