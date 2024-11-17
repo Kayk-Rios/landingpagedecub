6cbe4ccf52966ef190ccd204edc83ad58510dca0
Kayk-Rios <manjac688@gmail.com>
2024-11-17 01:43:14 -0300

    style:ajusteTamanho

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,10 @@ export default function Home() {
       <div className="flex flex-col ">
         <section className="flex justify-between items-center mb-14">
 
-          <div className="flex flex-col pl-[30px] ">
+          <div className="flex flex-col pl-[30px]  ">
              <a href="/layout" className="pb-9 text-white"><h1>Sobre</h1></a>
             <iframe
-
-              width="580"
+                className=" flex sm:-w[300px] md:w-[500px] lg:w-[650px] "
               height="415"
               src="https://www.youtube.com/embed/6wW-Z3KZnt4?si=eWTauZpI_Xxg1C7J"
               title="YouTube video player"
