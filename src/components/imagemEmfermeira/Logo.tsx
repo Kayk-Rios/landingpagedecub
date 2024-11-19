9bc182b21cad615ff72eaa2e52ab422850d056d7
Kayk-Rios <manjac688@gmail.com>
2024-11-19 14:38:49 -0300

    chore:ComponentLogo

---
+++ src/components/imagemEmfermeira/Logo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Decub from "@/assets/logoDecub.png";
+export default function Logo() {
+  return (
+    <div className="mr-[200px] ">
+      <a href="/">
+        <Image
+          src={Decub}
+          width={50}
+          height={50}
+          alt="ImagemDecubito"
+        />
+      </a>
+    </div>
+  );
+}
